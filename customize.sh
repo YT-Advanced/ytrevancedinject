b3130197a3b7c6e1fb15afd4962cd0626621ff2d
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2024-07-17 21:53:59 +0700

    feat: Add uninstall script

--- customize.sh
+++ customize.sh
@@ -8,12 +8,38 @@ if [ $BOOTMODE = false ]; then
 fi
 api_level_arch_detect
 
+
+# Process Monitor Tool (PMT) variables
+PMT_MODULE_PATH="/data/adb/modules/magisk_proc_monitor"
+PMTURL=https://github.com/HuskyDG/magisk_proc_monitor/releases/latest
+PMT_VERSION="$(grep_prop versionCode "$PMT_MODULE_PATH/module.prop")"
+
+# Check Process Monitor Tool
+[ ! -d "$PMT_MODULE_PATH" ] && {
+	ui_print "- Process monitor tool is not installed"
+	ui_print "  Please install it from $PMTURL"
+	am start -a android.intent.action.VIEW -d "$PMTURL" &>/dev/null
+	abort "- Aborting installation !!"
+}
+
+if [ "$PMT_VERSION" -lt 10 ]; then
+	ui_print "! Process Monitor Tool v2.3 or above is required"
+	am start -a android.intent.action.VIEW -d "$PMTURL" &>/dev/null
+	abort "- Aborting installation !!"
+fi
+
+if [ -f "$PMT_MODULE_PATH/disable" ] || [ -f "$PMT_MODULE_PATH/remove" ]; then
+	ui_print "! Process Monitor Tool is either be removed or disabled."
+	abort "- Aborting installation !!"
+fi
+
+
+# APK variables
 APPNAME="YouTube"
 PKGNAME="$(grep_prop package "$MODPATH/module.prop")"
 STOCKAPPVER=$(dumpsys package $PKGNAME | grep versionName | cut -d= -f 2 | sed -n '1p')
 RVAPPVER="$(grep_prop version "$MODPATH/module.prop")"
 URL="https://www.apkmirror.com/apk/google-inc/youtube/youtube-$(echo -n "$RVAPPVER" | tr "." "-")-release/"
-
 
 if [ ! -d "/proc/1/root/data/data/$PKGNAME" ]
 then
@@ -22,20 +48,12 @@ then
 	abort "- Aborting installation !!"
 fi
 
-[ ! -d "$/data/adb/modules/magisk_proc_monitor" ] && {
-    MURL=http://github.com/HuskyDG/magisk_proc_monitor
-    ui_print "- Process monitor tool is not installed"
-    ui_print "  Please install it from $MURL"
-    am start -a android.intent.action.VIEW -d "$MURL" &>/dev/null
-}
-
-
 if [ "$STOCKAPPVER" != "$RVAPPVER" ]
 then
 	ui_print "- Installed $APPNAME version = $STOCKAPPVER"
 	ui_print "- $APPNAME Revanced version = $RVAPPVER"
 	ui_print "- App Version Mismatch !!"
-  am start -a android.intent.action.VIEW -d "$URL" &>/dev/null
+	am start -a android.intent.action.VIEW -d "$URL" &>/dev/null
 	abort "- Aborting installation !!"
 fi
 
---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+#!/system/bin/sh
+
+[[ -e "/data/system/package_cache" ]] && rm -rf /data/system/package_cache/*; [[ -e "/data/adb/ytrevancedinject" ]] && rm -rf /data/adb/ytrevancedinject
